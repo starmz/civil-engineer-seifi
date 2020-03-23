0b66f5d613a9c1607623fd4885d21f9d64fd625d
star <mozaffari@sabacell.ir>
2020-03-24 02:27:50 +0430

    userprofile completed

--- src/Admin/AdminProfile.js
+++ src/Admin/AdminProfile.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './AdminProfile.css'
-import UserImage from '../Assets/Screenshot.jpg'
+import UserImage from '../Assets/beard_male_man_face.png'
 
 const AdminProfile = () => {
+  const imageStyle = {
+    backgroundImage: `url(${UserImage})`
+  }
   return (
 	<section className="admin-profile">
-    <div class="admin-profile__image-wrapper">
-      <img alt="userimage" src={ UserImage } />
-    </div>
+    <div className="admin-profile__image" style={ imageStyle } />
     <h1>Setareh Mozaffari</h1>
+    <small>senior front-end developer</small>
+    <p admin-profile__description className="admin-profile__description">I’m Setareh Mozaffari a senior front-end developer from Tehran, IRAN.</p>
 	</section>
   )
 }
